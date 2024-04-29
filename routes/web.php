82426a0add2509e44a971600ca9a3010c78a5074
thiagorezendev <thiagor3zende@gmail.com>
2024-04-28 22:37:34 -0300

    Adicionando rota de fallback

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,16 @@ Route::prefix('/app')->group(function(){
     Route::get('/produtos', function(){ return 'Produtos'; })->name('app.produtos');
 });
 
+Route::get('/rota1', function() {
+    echo 'Rota 1';
+})->name('site.rota1');
+
+Route::get('/rota2', function() {
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+// Route::redirect('/rota2', '/rota1');
+
+Route::fallback(function() {
+    echo 'A rota acessada não existe! <a href="'.route('site.index').'">Clique aqui</a> para voltar para a página principal.';
+});
