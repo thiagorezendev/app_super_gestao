8c7c1faffd619e20107e162edcc523384f62c051
thiagorezendev <thiagor3zende@gmail.com>
2024-08-01 17:02:34 -0300

    enviando form via get

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 class ContatoController extends Controller
 {
     public function contato() {
+
+        var_dump($_GET);
         return view('site.contato');
     }
 }
