0b73470f5d8e21849360b5892c2e387cb6e87bff
thiagorezendev <thiagor3zende@gmail.com>
2024-09-02 08:37:24 -0300

    migrate

--- database/migrations/2024_08_24_232507_create_site_contatos_table.php
+++ database/migrations/2024_08_24_232507_create_site_contatos_table.php
@@ -14,6 +14,11 @@ return new class extends Migration
         Schema::create('site_contatos', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('nome', length: 100);
+            $table->string('telefone', length: 20);
+            $table->string('email', length: 50);
+            $table->integer('motivo_contato');
+            $table->text('mensagem');
         });
     }
 
