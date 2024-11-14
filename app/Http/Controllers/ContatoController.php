8aaf10e043be9452664103d2a9d2bce87917f1ed
thiagorezendev <thiagor3zende@gmail.com>
2024-11-14 17:58:37 -0300

    avançando em forms 3

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -17,13 +17,26 @@ class ContatoController extends Controller
     public function salvar(Request $request){
         
         //validar os dados do formulário recebido no request
-        $request->validate([
-            'nome' => 'required|min:3|max:40',
+        $regras = [
+            'nome' => 'required|min:3|max:40|unique:site_contatos',
             'telefone' => 'required',
             'email' => 'email',
             'motivo_contatos_id' => 'required',
             'mensagem' => 'required|max:2000'
-        ]);
+        ];
+
+        $feedback = [
+            'nome.min' => 'O campo nome deve ter no mínimo 3 caracteres.',
+            'nome.max' => 'O campo nome deve ter no máximo 40 caracteres.',
+            'nome.unique' => 'O nome informado já está em uso.',
+            'email.email' => 'O campo email informado não é válido.',
+            'mensagem.max' => 'O campo mensagem deve ter no máximo 2000 caracteres.',
+
+            'required' => 'O campo :attribute deve ser preenchido.'
+        ];
+
+        $request->validate($regras, $feedback);
+
         SiteContato::create($request->all());
         return redirect()->route('site.index');
     }
